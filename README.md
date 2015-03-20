1d95445c7fbf58779ca765dbb3488bf8a34d45c7
Shane Eckenrode <sue2@psu.edu>
2015-03-20 15:22:54 -0400

    Added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+#exercise6
+Exercise for IST297I
+Shane Eckenrode
+3/20/15
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227195104) do
+ActiveRecord::Schema.define(version: 20150320190014) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
